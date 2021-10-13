9d0c3dd022651558260960bfc06f26f19d006dd1
btcm31 <congminhabcd@gmail.com>
2021-10-13 22:42:23 +0700

    add symptom scenarion

--- backend/classifierbackend/reply.py
+++ backend/classifierbackend/reply.py
@@ -16,24 +16,65 @@ with open(path + '\\image\\emergycontact\\contact.json', encoding='utf-8') as f:
 
 
 state_conversation = 'Hello'
+pre_intent = ""
 #state này để lưu những thông tin trong từng kịch bản
 state = {
+    "infor_patient": {
+        "name": "",
+        "age": "",
+        "sex": "",
+        "phone": ""
+    },
     "emergency_contact": {
         "who": "",
         "location": ""
     },
-    "symptom": {},
+    "symptom": {
+        "usually": {
+            "sot": "",
+            "met-moi": "",
+            "ho": ""
+        },
+        "serious": {
+            "kho-tho": "",
+            "tuc-nguc": "",
+            "mat-kha-nang": ""
+        },
+        "rarely": {
+            "dau-hong": "",
+            "dau-nhuc": "",
+            "tieu-chay": "",
+            "mat-vi-giac": "",
+            "tim-tai": "",
+            "noi-man": ""
+        }
+    },
     "precaution": {},
     "infor": {},
     "medication": {},
     "vacxin": {}
 }
 def symptom(text):
-    entity_lst = ['ho', 'sốt', 'mệt mỏi', 'ho khan', 'đau họng', 'đau họng', 'đau nhức', 'tiêu chảy', 'mất vị giác', 'mất khứu giác', 'da nổi mẩn', 'tấy đỏ', 'tím tái', 'khó thở', 'mất khả năng nói hoặc cử động', 'tức ngực']
-    entity_in_text = []
-    for ent in entity_lst:
-        if ent in text:
-            entity_in_text.append(ent)
+    entity_lst = ['ho', 'sốt', 'mệt mỏi', 'đau họng', 'đau nhức', 'tiêu chảy', 'mất vị giác', 'nổi mẩn'
+                    , 'tím tái', 'khó thở', 'mất khả năng', 'tức ngực']
+    symptom_reg = r'(ho|s[ô|ố|ó]t|m[ệ|ẹ|e|ê]t\s*m[o|ỏ]i|[đ|d]au\s*h[o|ọ]ng|kh[o|ó]\s*th[ơ|ở|ỏ]|[đ|d]au\s*nh[ư|ú|ứ]c|ti[ê|e]*u\s*ch[a|ả]y|\
+                    |m[a|ấ|â|á]t\s*v[i|ị]\s*gi[a|á]c|n[ô|ổ|ỏ]i\s*m[a|ả|ẩ|â]n|t[i|í]m\s*t[a|á]i|t[ư|ứ|u|ú]c\s*ng[ụ|ự|ư|u]c)'
+    entity_in_text = re.findall(symptom_reg, text)
+    severity = ['serious','usually', 'rarely']
+    print(entity_in_text)
+    for symp in entity_in_text:
+        for ele in state['symptom']:
+            if '-'.join(unidecode(symp).split()) in state['symptom'][ele].keys():
+                state['symptom'][ele]['-'.join(unidecode(symp).split())] = 1
+                
+    for ele in state['symptom']:
+            if any([state['symptom'][ele][i] for i in state['symptom'][ele]]):
+                if ele in severity:
+                    severity.remove(ele)
+    if not severity:
+        return 'Khả năng nhiễm của bạn khá cao á, bạn hãy liên hệ với các trung tâm y tế để test covid thử nha.'
+
+    return reply_dic['Request']['symptom_have'][severity[0]]
 
 def common_information(text):
     pass
@@ -84,7 +125,7 @@ def emergency_contact(text):
     else:
         print(['-'.join(unidecode(location[0]).split())])
         print('-'.join(unidecode(contact[0]).split()))
-        reply = ['Bạn liện hệ theo một trong những số điện thoại dưới đây nha']
+        reply = ['Bạn liên hệ theo một trong những số điện thoại dưới đây nha']
         reply.append(contact_emergency['-'.join(unidecode(contact[0]).split())]['-'.join(unidecode(location[0]).split())])
     
     return reply
@@ -100,22 +141,22 @@ def reply(text):
     response = ""
 
     #Nếu bệnh nhân request intent nào thì chuyển state hội thoại sang hướng đó
-    if intent == 'Request':
+    if intent in ['Request', 'Inform']:
         state_conversation = intent_request
 
     if intent in ['Request', 'Inform']:
         if state_conversation == 'emergency_contact':
             response = emergency_contact(text)
-        elif state_conversation == 'symptom':
-            pass
-        elif state_conversation == 'infor':
-            pass
-        elif state_conversation == 'precaution':
+        elif state_conversation == 'symptom_have':
+            response = symptom(text)
+        elif state_conversation == 'precautions':
             pass
         elif state_conversation == 'medication':
             pass
         elif state_conversation == 'vacxin':
             pass
+        else:
+            return reply_dic["Request"][state_conversation]
     else:
         response = reply_dic[intent]
 
--- backend/test.py
+++ backend/test.py
@@ -1,5 +1,5 @@
 import regex as re
 
-localtion_reg = r'[quâậan]+\s*([1-9]+|b[i|ì]nh\s*th[a|ạ]nh|t[a|â]n\s*ph[u|ú]|b[i|ì]nh\s*t[a|â]n|b[i|ì]nh\s*ch[a|á]nh|nh[a|à]\s*b[e|è]|g[o|ò]\s*v[a|á|ấ|â]p|ph[u|ú]\s*nhu[a|ạ|â|ậ]n|t[a|â]n\s*b[i|ì][|n]h|th[u|ủ]\s[d|đ][u|ú|ư|ứ]c|c[a|à|ầ|â]n\s*gi[o|ơ|ờ]|c[u|ủ]\schi|h[o|ó]c\s*m[o|ô]n)'
-    
-print(re.findall(localtion_reg, 'em ở qạn 12as '))
+localtion_reg = r'(ho|s[ô|ố|ó]t|m[ệ|ẹ|e|ê]t\s*m[o|ỏ]i|[đ|d]au\s*h[o|ọ]ng|kh[o|ó]\s*th[ơ|ở|ỏ]|[đ|d]au\s*nh[ư|ú|ứ]c|ti[ê|e]*u\s*ch[a|ả]y|\
+                    |m[a|ấ|â|á]t\s*v[i|ị]\s*gi[a|á]c|n[ô|ổ|ỏ]i\s*m[a|ả|ẩ|â]n|t[i|í]m\s*t[a|á]i|t[ư|ứ|u|ú]c\s*ng[ụ|ự|ư|u]c)'
+print(re.findall(localtion_reg, 'em bị khó thở với đau họng đau nhức tiu chảy mất vị giác tím tái á'))
